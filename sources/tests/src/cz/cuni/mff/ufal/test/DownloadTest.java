9b87adb2293bc957eb1f322fc6d8cc539bf21e8e
Loganathan Ramasamy <loganathanspr@gmail.com>
2014-09-19 16:28:01 +0200

    Testing if a file download give a 'file not found' error

--- sources/tests/src/cz/cuni/mff/ufal/test/DownloadTest.java
+++ sources/tests/src/cz/cuni/mff/ufal/test/DownloadTest.java
@@ -1,27 +1,42 @@
-/**
- * 
- */
 package cz.cuni.mff.ufal.test;
 
+import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
 
-/**
- * @author ramasamy
- *
- */
 public class DownloadTest extends BaseTestCase {
 	
 	@Test
 	public void testFilesAvailableForDownload() {
 		selenium.open(prop.getProperty("selenium.test.home.url"));
 		selenium.waitForPageToLoad("30000");
+
+		// Advanced Search
 		selenium.click("link=Advanced Search");
 		selenium.waitForPageToLoad("30000");
+		
+		// click "Contain files" at left search filter panel
 		selenium.click("//div[@id='search-filters']/div[6]/div/a[@href='#aspect_discovery_SidebarFacetsTransformer_list_hasfile']");
-		//selenium.click("//div[@id='aspect_discovery_SidebarFacetsTransformer_list_hasfile']/div/ul/li/ul/li[2]/a/span");
-		selenium.waitForPageToLoad("30000");
-		//*[@id="search-filters"]/div[6]/div[1]/a
+		selenium.waitForPageToLoad("1000");
 
-	}
-	
+		// click "Yes"
+		selenium.click("//div[@id='aspect_discovery_SidebarFacetsTransformer_list_hasfile']/div/ul/li/ul/li[1]/a/span");
+		selenium.waitForPageToLoad("30000");
+
+		// click the first item
+		selenium.click("//li[1]/div[@class='artifact-title']/a");
+		selenium.waitForPageToLoad("30000");
+		String itemUrl= selenium.getLocation();
+
+		//store the file name to be downloaded
+		String fileName = selenium.getText("//div[@id='files_section']/div[@class='thumbnails']/div[1]/dl/dd[1]");
+
+		// click the first "download" link	
+		selenium.click("//div[@id='files_section']/div[@class='thumbnails']/div[1]/a[2]");
+		selenium.waitForPageToLoad("30000");
+		
+		if (selenium.isTextPresent("File not found")) {
+			AssertJUnit.fail("File not found !!! Downloading unsuccessful (filename:" +  fileName  + ") for the item at this location -> " + itemUrl);
+		}
+		
+	}	
 }
