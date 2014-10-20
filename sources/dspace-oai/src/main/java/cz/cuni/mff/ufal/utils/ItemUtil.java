fe5164151aed2611a874db2296f7d16097c978b5
Ondřej Košarko <kosarko@ufal.mff.cuni.cz>
2014-10-20 15:32:24 +0200

    different profile for new submissions

--- sources/dspace-oai/src/main/java/cz/cuni/mff/ufal/utils/ItemUtil.java
+++ sources/dspace-oai/src/main/java/cz/cuni/mff/ufal/utils/ItemUtil.java
@@ -8,8 +8,10 @@ import java.io.Writer;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.commons.io.IOUtils;
+import org.dspace.app.util.DCInput;
 import org.dspace.content.Bitstream;
 import org.dspace.content.DSpaceObject;
 import org.dspace.content.Item;
@@ -17,6 +19,7 @@ import org.dspace.core.Constants;
 import org.dspace.core.Context;
 import org.dspace.handle.HandleManager;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.xml.sax.InputSource;
 
@@ -57,6 +60,94 @@ public class ItemUtil {
 			closeContext(context);
 		}
 		return ret;
+	}
+	
+	public static Node getFunding(String mdValue){
+		String ns = "http://www.clarin.eu/cmd/";
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		factory.setNamespaceAware(true);
+		DocumentBuilder builder;
+		try {
+			builder = factory.newDocumentBuilder();
+			Document doc = builder.newDocument();
+			Element el = doc.createElementNS(ns, "funding");
+			doc.getDocumentElement().appendChild(el);
+			Element organization = doc.createElementNS(ns, "organization");
+			Element projName = doc.createElementNS(ns, "projectName");
+			Element code = doc.createElementNS(ns, "code");
+			Element fundsType = doc.createElementNS(ns, "fundsType");
+
+			String[] values = mdValue
+					.split(DCInput.ComplexDefinition.SEPARATOR);
+
+			// mind the order in input forms, org;code;projname;type
+			Element[] elements = { organization, code, projName, fundsType };
+			for (int i = 0; i < values.length; i++) {
+				elements[i].setNodeValue(values[i]);
+				el.appendChild(elements[i]);
+			}
+			return doc.getDocumentElement();
+		} catch (ParserConfigurationException e) {
+			return null;
+		}
+	}
+
+	public static Node getContact(String mdValue){
+		String ns = "http://www.clarin.eu/cmd/";
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		factory.setNamespaceAware(true);
+		DocumentBuilder builder;
+		try {
+			builder = factory.newDocumentBuilder();
+			Document doc = builder.newDocument();
+			Element el = doc.createElementNS(ns, "contactPerson");
+			doc.getDocumentElement().appendChild(el);
+			Element first = doc.createElementNS(ns, "firstName");
+			Element last = doc.createElementNS(ns, "lastName");
+			Element email = doc.createElementNS(ns, "email");
+			Element affil = doc.createElementNS(ns, "affiliation");
+
+			String[] values = mdValue
+					.split(DCInput.ComplexDefinition.SEPARATOR);
+
+			// mind the order in input forms, org;code;projname;type
+			Element[] elements = { first, last, email, affil };
+			for (int i = 0; i < values.length; i++) {
+				elements[i].setNodeValue(values[i]);
+				el.appendChild(elements[i]);
+			}
+			return doc.getDocumentElement();
+		} catch (ParserConfigurationException e) {
+			return null;
+		}
+	}
+
+	public static Node getSize(String mdValue){
+		String ns = "http://www.clarin.eu/cmd/";
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		factory.setNamespaceAware(true);
+		DocumentBuilder builder;
+		try {
+			builder = factory.newDocumentBuilder();
+			Document doc = builder.newDocument();
+			Element el = doc.createElementNS(ns, "size");
+			doc.getDocumentElement().appendChild(el);
+			Element size = doc.createElementNS(ns, "size");
+			Element unit = doc.createElementNS(ns, "unit");
+
+			String[] values = mdValue
+					.split(DCInput.ComplexDefinition.SEPARATOR);
+
+			// mind the order in input forms, org;code;projname;type
+			Element[] elements = {size, unit};
+			for (int i = 0; i < values.length; i++) {
+				elements[i].setNodeValue(values[i]);
+				el.appendChild(elements[i]);
+			}
+			return doc.getDocumentElement();
+		} catch (ParserConfigurationException e) {
+			return null;
+		}
 	}
 	
 	private static void closeContext(Context c){
