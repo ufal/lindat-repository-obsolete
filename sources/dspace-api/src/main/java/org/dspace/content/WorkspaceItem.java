93de5d7a2f3b2a2936a6dbba128df9015273af82
Amir Kamran <amirkamran@msn.com>
2014-11-04 16:18:50 +0100

    correction in branding community vs collection name

--- sources/dspace-api/src/main/java/org/dspace/content/WorkspaceItem.java
+++ sources/dspace-api/src/main/java/org/dspace/content/WorkspaceItem.java
@@ -249,7 +249,7 @@ public class WorkspaceItem implements InProgressSubmission
             }
         }
 
-	i.addMetadata("local", "branding", null, null, coll.getPrincipalCommunity().getName());
+        i.addMetadata("local", "branding", null, null, coll.getPrincipalCommunity().getName());
         
         i.update();
 
