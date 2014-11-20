457edf685c6b125ca73e05323dca0234d40361f5
Michal Josífko <josifko@ufal.mff.cuni.cz>
2014-11-20 15:17:52 +0100

    Added support for correct tracking of IP address via Piwik.

--- sources/dspace-api/src/main/java/cz/cuni/mff/ufal/tracker/PiwikBitstreamTracker.java
+++ sources/dspace-api/src/main/java/cz/cuni/mff/ufal/tracker/PiwikBitstreamTracker.java
@@ -14,6 +14,7 @@ public class PiwikBitstreamTracker extends PiwikTracker
 
     @Override
     protected void preTrack(HttpServletRequest request) {
+        super.preTrack(request);
         tracker.setIdSite(getIdSite());
         try
         {
